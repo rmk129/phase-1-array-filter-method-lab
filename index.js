3a7257122307944cb3cace9eaeac78acb130a49b
Romil <romil.m.kothari@gmail.com>
2023-03-22 11:05:35 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let dNames = drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+
+   return dNames
+}
+
+function fuzzyMatch(drivers, name) {
+    let dNames = drivers.filter(function(driver) {
+        return driver.slice(0,2).toLowerCase() === name.toLowerCase();
+    })
+    return dNames
+}
+
+function matchName(drivers, names){
+    let dNames = drivers.filter(function(driver){
+        if (driver.name.toLowerCase() === names.toLowerCase()){
+            return driver;
+        }
+    })
+    return dNames;
+}
